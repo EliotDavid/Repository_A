6ec958bbd699f1bbe019ea28cf7b20dc0be73474
EliotDavid <optimistprimer@naver.com>
2022-11-24 13:47:46 +0900

    2022_11_24(teacher_ver)

---
+++ 2022_11_24(teacher_ver)/src/main/java/com/example/demo/relation/domain/member/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.relation.domain.member;
+
+import com.example.demo.relation.domain.academy.Academy;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+}
---
+++ 2022_11_24(teacher_ver)/src/main/java/com/example/demo/relation/view/member/RelationController.java
@@ -0,0 +1,71 @@
+package com.example.demo.relation.view.member;
+
+import com.example.demo.relation.domain.academy.Academy;
+import com.example.demo.relation.domain.academy.AcademyRepository;
+import com.example.demo.relation.domain.member.Member;
+import com.example.demo.relation.domain.service.MemberService;
+import com.example.demo.relation.view.member.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequestMapping("/members")
+@RequiredArgsConstructor
+@Controller
+public class RelationController {
+    private final MemberService memberService;
+    private final AcademyRepository academyRepository;
+
+    @GetMapping("/new")
+    public String insert(@ModelAttribute("form") MemberDto dto) {
+        return "members/newMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String save(@Valid @ModelAttribute("form") MemberDto dto) {
+
+        List<Academy> academies = academyRepository.findByName(dto.getAcademyName());
+
+        Academy academy = null;
+
+        if(!academies.isEmpty())
+            academy = academies.get(0);
+        else
+            academy = new Academy(dto.getAcademyName());
+
+        memberService.insert(
+                new Member(
+                        dto.getLoginId(),
+                        dto.getMemberName(),
+                        dto.getPassword(),
+                        academy) );
+
+        /*
+        List<Academy> all = academyRepository.findAll();
+        if(all.isEmpty())
+        {
+            Academy academy = new Academy(dto.getAcademyName());
+            memberService.insert(
+                    new Member( dto.getMemberName(), academy));
+        }
+        else
+        {
+            for (Academy element : all) {
+                if(element.getAcademyName().equals(dto.getAcademyName())) {
+                    Academy academy = academyRepository.findById(element.getId());
+                    memberService.insert(
+                            new Member( dto.getMemberName(), academy));
+                }
+            }
+        }
+*/
+
+        return "redirect:/";
+    }
+}
